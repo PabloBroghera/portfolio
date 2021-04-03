1a98832b1ba69b7e2fd6ed506d91d52877c51e30
PabloBroghera <pablobroghera2000@gmail.com>
2021-04-03 17:36:41 -0300

    cambios3

--- menu.php
+++ menu.php
@@ -8,16 +8,16 @@
             <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                 <ul class="navbar-nav mr-auto text-center">
                     <li class="nav-item pr-5">
-                        <a class="nav-link <?php echo $pg == "inicio"? "active": "";"" ?>href="index.php">Inicio</a>
+                        <a class="nav-link <?php echo $pg == "inicio"? "active": "";"" ?>" href="index.php">Inicio</a>
                     </li>
                     <li class="nav-item pr-5">
-                        <a class="nav-link <?php echo $pg == "sobre-mi"? "active": "";"" ?> href="sobre-mi.php">Sobre mí</a>
+                        <a class="nav-link <?php echo $pg == "sobre-mi"? "active": "";"" ?>" href="sobre-mi.php">Sobre mí</a>
                     </li>
                     <li class="nav-item pr-5">
-                        <a class="nav-link <?php echo $pg == "proyectos"? "active": "";"" ?> href="proyectos.php">Proyectos</a>
+                        <a class="nav-link <?php echo $pg == "proyectos"? "active": "";"" ?>" href="proyectos.php">Proyectos</a>
                     </li>
                     <li class="nav-item pr-5">
-                        <a class="nav-link <?php echo $pg == "contacto"? "active": "";"" ?> href="contacto.php">Contacto</a>
+                        <a class="nav-link <?php echo $pg == "contacto"? "active": "";"" ?>" href="contacto.php">Contacto</a>
                     </li>
                 </ul>
                 <div class="form-inline my-2 my-lg-0">
